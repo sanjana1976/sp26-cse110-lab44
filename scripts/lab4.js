28d6eedf1160379188a16317ec728777cffb1bb8
sanjana1976 <sanjanakjose@gmail.com>
2026-04-28 23:42:07 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,6 +7,9 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            return false;
+        }
         let result = 0;
 
         result = num1 + num2;
@@ -25,6 +28,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || prices.length === 0) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
